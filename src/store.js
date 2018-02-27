4d51af957f4f61f644f3544d74c3e28996f7b784
Pawel Galwa <pawelgalwa@Pawels-MacBook-Pro.local>
2018-02-27 20:11:51 +0100

    rewritten autocomplete method, added checking 'light conditions'

--- src/store.js
+++ src/store.js
@@ -3,21 +3,51 @@ import fetch from 'cross-fetch';
 
 const COLORS_URL = 'http://www.mocky.io/v2/5a37a7403200000f10eb6a2d';
 const START_SEARCH = 2;
+const MAX_SEARCH_ARR_SIZE = 10;
 
 export class MainStore {
   @observable colors = [];
   @observable searchInput = '';
   @observable backgroundColor = '';
-  @observable state = "pending"
-
-  autocomplete() {
-    return this.colors.filter((c)=>c.name.indexOf(this.searchInput) !== -1);
-  }
+  @observable state = "pending";
 
   constructor(initData) {
     if(initData && initData.length > 0) {
       this.colors = initData;
     }
+  }
+
+  autocomplete() {
+    const strongCondition = (c, input) => c.name.includes(input);
+
+    // The reason why I wrote that 'magic function' is simple - there is no default method for checking
+    // if *every* element from one array exists in second one.
+    // i.e 'if [1, 2] is in [1, 3, 4]'* condition should return false because not every element from first array is in second one
+    // but 'if [1, 2] is in [1, 3, 2]' should return true because both 1, 2 we can find in second one
+    // Cuz strings are arrays, we can map and check if every element from the input exists in color.name property.
+    // If there is at least one 'false' result, we can assume that not every character from the input is in color.name property.
+    //
+    // pure functional approach -> !input.split('').map(e=>c.name.includes(e)).includes(false);
+    // finally I used a bit more imperative approach because it's a bit faster and easier to read
+    // *pseudocode
+
+    const lightCondition = (c, input) => {
+      const inputArr = input.split('');
+      for(let idx in inputArr) {
+        if(!c.name.includes(inputArr[idx])) {
+          return false;
+        }
+      }
+      return true;
+    }    
+
+    const createEntriesArr = (arr, input, filterFunc) => arr.filter(c => filterFunc(c, input));
+    
+    return [...new Set([
+      ...createEntriesArr(this.colors, this.searchInput, strongCondition), 
+      ...createEntriesArr(this.colors, this.searchInput, lightCondition)
+    ])]; // union of two arrays cuz entries can repeat
+
   }
 
   @computed get entriesList() {
